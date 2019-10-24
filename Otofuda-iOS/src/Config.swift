2cee1d0643636cc612dcad158668670934af1a91
nshhhin <shinn070110@gmail.com>
2019-10-24 14:09:23 +0900

    :sparkles: バージョンアップで開放されるものを選択不可にした

--- Otofuda-iOS/src/Config.swift
+++ Otofuda-iOS/src/Config.swift
@@ -4,4 +4,5 @@ import Foundation
 class Config {
     static let fudaMaxCount = 16
     static let colors: [MyColor] = [.red, .blue, .green, .pink, .brown, .yellow, .orange]
+    static let list_api_url = "https://uniotto.org/api/get_all_otofuda_list.php"
 }

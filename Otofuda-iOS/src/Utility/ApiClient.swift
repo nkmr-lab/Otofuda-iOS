266b5b1e878f21f0815a158364991f1c225d8c69
nshhhin <shinn070110@gmail.com>
2019-10-24 14:02:32 +0900

    :heavy_plus_sign: RxSwiftを追加

---
+++ Otofuda-iOS/src/Utility/ApiClient.swift
@@ -0,0 +1,68 @@
+//
+//  ApiClient.swift
+//  Otofuda-iOS
+//
+//  Created by 新納真次郎 on 2019/10/24.
+//  Copyright © 2019 nkmr-lab. All rights reserved.
+//
+
+import SwiftyJSON
+import Alamofire
+import RxSwift
+
+protocol ApiClientProtocol {
+    var baseUrl: String { get }
+
+    init(_ baseUrl: String)
+
+    func get<T: ResponseEntity>(path: String?,
+                                request: RequestEntity?) -> Single<T>
+}
+
+class ApiClient: ApiClientProtocol {
+
+    var baseUrl: String
+
+    required init(_ baseUrl: String = BASE_API_URL) {
+        self.baseUrl = baseUrl
+    }
+
+    func get<T: ResponseEntity>(path: String?,
+                                request: RequestEntity?) -> Single<T> {
+
+        var requestUrl = baseUrl
+        if let path = path {
+            requestUrl = baseUrl + path
+        }
+
+        var params = Parameters()
+        if let request = request {
+            params = request.parameterize()
+        }
+
+        return Single<T>.create { single in
+            let manager = SessionManager.default
+            let request = manager.request(requestUrl,
+                                          method: .get,
+                                          parameters: params,
+                                          encoding: URLEncoding.default,
+                                          headers: nil)
+                .responseJSON(completionHandler: { response in
+                    switch response.result {
+                    case .success(_):
+                        guard let data = response.data else {
+                            return single(.error(response.error!))
+                        }
+                        let json = JSON(data)
+                        return single(.success(T(json)))
+
+                    case .failure(let error):
+                        return single(.error(error))
+                    }
+                })
+            return Disposables.create {
+                return request.cancel()
+            }
+        }
+    }
+}
